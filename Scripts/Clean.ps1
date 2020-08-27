8847328afa8ea79d3cac8945fdbdd38a0d5a0607
Jlobblet <jlobblet@gmx.com>
2020-08-27 23:19:37 +0100

    Add Clean and Smudge scripts

---
+++ Scripts/Clean.ps1
@@ -0,0 +1,4 @@
+﻿foreach ($file in $args)
+{
+    (Get-Content "$file") -replace '<RepositoryUrl>D:\\.nuget\\Local</RepositoryUrl>', '<RepositoryUrl>{insert repository url here}</RepositoryUrl>' | Write-Host
+}
---
+++ Scripts/Smudge.ps1
@@ -0,0 +1,4 @@
+﻿foreach ($file in $args)
+{
+    (Get-Content "$file") -replace '<RepositoryUrl>{insert repository url here}<\/RepositoryUrl>', '<RepositoryUrl>D:\.nuget\Local</RepositoryUrl>' | Write-Host
+}
